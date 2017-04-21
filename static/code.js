4ae764f52f98fb72589da4d63fea42628ca80ba4
admin <@>
2017-04-21 16:09:59 -0400

    Got the duke graph to load

--- static/code.js
+++ static/code.js
@@ -1,25 +1,5 @@
-function init(){ // on dom ready
+function init(graph){ // on dom ready
 
-  var elesJson = {
-    nodes: [
-      { data: { id: 'a', foo: 3, bar: 5, baz: 7 } },
-      { data: { id: 'b', foo: 7, bar: 1, baz: 3 } },
-      { data: { id: 'c', foo: 2, bar: 7, baz: 6 } },
-      { data: { id: 'd', foo: 9, bar: 5, baz: 2 } },
-      { data: { id: 'e', foo: 2, bar: 4, baz: 5 } }
-    ], 
-
-    edges: [
-      { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
-      { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
-      { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
-      { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
-      { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
-      { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
-      { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
-    ]
-  };
-  
   var cy = cytoscape({
     container: document.getElementById('cy'),
     
@@ -27,8 +7,6 @@ function init(){ // on dom ready
                     .selector('node')
                     .css({
                       'background-color': '#B3767E',
-                      'width': 'mapData(baz, 0, 10, 10, 40)',
-                      'height': 'mapData(baz, 0, 10, 10, 40)',
                       'content': 'data(id)'
                     })
                     .selector('edge')
@@ -53,7 +31,7 @@ function init(){ // on dom ready
                       'text-opacity': 0
                     }),
     
-    elements: elesJson,
+    elements: graph,
     
     layout: {
       name: 'circle',
@@ -65,6 +43,6 @@ function init(){ // on dom ready
     },
 
     maxZoom: 2,
-    minZoom: 0.5
+    minZoom: 0.05
   });
 }; // on dom ready

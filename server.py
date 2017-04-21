d4d7f58eb1036cb8a38b7c10f909094db67423e7
admin <@>
2017-04-20 23:09:55 -0400

    Added static serving of files

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/<string:page_name>/')
+def static_page(page_name):
+    return render_template('%s.html' % page_name)

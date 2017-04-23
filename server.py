45ceddeb1e28a7c8cd4c26be0c59c6886f94def3
admin <@>
2017-04-22 23:28:26 -0400

    Added a base template and made family use it

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/family/')
+@app.route("/family/")
 def get_families():
-    return render_template('family.html')
+    return render_template("family.html", title="Families")
 
 # Add bootstrap, split into 4
 # Create a template for the table

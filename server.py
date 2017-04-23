1b599a168f278c14126247ef4edf2aabe55dfc56
admin <@>
2017-04-22 22:44:53 -0400

    Initial family layout, plus jquery for bootstrap

--- server.py
+++ server.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/family/')
+def get_families():
+    return render_template('family.html')
+
+# Add bootstrap, split into 4
+# Create a template for the table
+# Add routes for family to species
+# Pickle objects for this

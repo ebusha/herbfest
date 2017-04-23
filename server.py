7e59e8d606ef94d8a5e3d113ab9cb92720941853
admin <@>
2017-04-23 13:11:31 -0400

    Added the lists for family, genus, and species

--- server.py
+++ server.py
@@ -14,13 +14,48 @@ def index():
 @app.route("/family/")
 def get_families():
     return render_template("family.html",
-                           title="Families",
+                           title="Family",
                            results=db.execute('''
                            SELECT family, COUNT(family)
                            FROM fgs GROUP BY family
                            ''').fetchall())
 
-# Add bootstrap, split into 4
-# Create a template for the table
-# Add routes for family to species
-# Pickle objects for this
+
+@app.route("/family/<family>/")
+def get_family_genus(family):
+    return render_template("genus.html",
+                           title="Genus",
+                           f=family,
+                           families=db.execute('''
+                           SELECT family, COUNT(family)
+                           FROM fgs GROUP BY family
+                           ''').fetchall(),
+                           genera=db.execute('''
+                           SELECT genus, COUNT(genus)
+                           FROM fgs
+                           WHERE family=="%s"
+                           GROUP BY genus
+                           ''' % family).fetchall())
+
+
+@app.route("/family/<family>/<genus>/")
+def get_family_genus_species(family, genus):
+    return render_template("species.html",
+                           title="Genus",
+                           f=family,
+                           families=db.execute('''
+                           SELECT family, COUNT(family)
+                           FROM fgs GROUP BY family
+                           ''').fetchall(),
+                           g=genus,
+                           genera=db.execute('''
+                           SELECT genus, COUNT(genus)
+                           FROM fgs
+                           WHERE family=="%s"
+                           GROUP BY genus
+                           ''' % family).fetchall(),
+                           species=db.execute('''
+                           SELECT species
+                           FROM fgs
+                           WHERE family=="%s" and genus=="%s"
+                           ''' % (family, genus)).fetchall())
